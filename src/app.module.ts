ce4f41001755a12d9844658ea5e71ab71129f90e
Cristian Castro <kencrisgdev@gmail.com>
2026-05-15 16:47:11 -0500

    feat: initialize wallet module, configure TypeORM database connection, and add environment variable template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgrespassword',
+      database: 'fintech_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    WalletModule,
+    
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
