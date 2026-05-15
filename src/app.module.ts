7b1abdd334efd7760742ab4052660493e1598fec
Cristian Castro <kencrisgdev@gmail.com>
2026-05-15 17:02:48 -0500

    feat: configure TypeORM read/write database connections in AppModule and update environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,48 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WalletModule } from './wallet/wallet.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgrespassword',
-      database: 'fintech_db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true, // Permite usar ConfigService en cualquier módulo sin importarlo
     }),
-    WalletModule,
-    
+    // 1. CONEXIÓN POR DEFECTO (Escritura - fintech_w)
+    // Al no ponerle un 'name', TypeORM la toma como la principal.
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_WRITE_HOST'),
+        port: configService.get<number>('DB_WRITE_PORT'),
+        username: configService.get<string>('DB_WRITE_USERNAME'),
+        password: configService.get<string>('DB_WRITE_PASSWORD'),
+        database: configService.get<string>('DB_WRITE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      
+    }),
+    // 2. CONEXIÓN NOMBRADA (Lectura - fintech_r)
+    // Usaremos el nombre 'readConnection' para inyectarla en los Repositorios de Lectura.
+    TypeOrmModule.forRootAsync({
+      name: 'readConnection', // <--- ESTO ES VITAL
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_READ_HOST'),
+        port: configService.get<number>('DB_READ_PORT'),
+        username: configService.get<string>('DB_READ_USERNAME'),
+        password: configService.get<string>('DB_READ_PASSWORD'),
+        database: configService.get<string>('DB_READ_NAME'),
+        entities: [__dirname + '/**/*.view{.ts,.js}'], // Buenas prácticas: separar vistas de entidades
+        synchronize: true, 
+      }),
+    }),
+    WalletModule
   ],
   controllers: [],
   providers: [],
